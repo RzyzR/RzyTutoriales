7c32f02e73d05711e5ede858072dfff930f78b4f
RzyzR <52755789+RzyzR@users.noreply.github.com>
2023-01-23 13:26:46 -0500

    Create Conectar.java

---
+++ Java/CRUD MVC/Modelo/Conectar.java
@@ -0,0 +1,30 @@
+package CRUD_MVC.Modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Randy
+ */
+public class Conectar {
+    public static final String URL = "jdbc:mysql://localhost:3306/name_db"; //"jdbc:mysql://localhost:3306/name_db?autoReconnet=true&useSSL=false"
+    public static final String USER = "user";
+    public static final String PASSWORD = "password";
+    
+    Connection c = null;
+    
+    public Conectar() {
+    }
+    
+    public Connection getConection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =  DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: "+e);
+        }
+        return c;
+    }
+}
